7d0fb481b947d33114c0626fc45b06d913eb39a7
Alphiletti <67262963+Alphiletti@users.noreply.github.com>
2021-12-14 15:32:00 +0300

    Add files via upload

---
+++ konsol-programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace konsol_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi Girin:");
+            string fname = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Girin:");
+            string sname = Console.ReadLine();
+            Console.WriteLine($"Merhaba {fname} {sname} ");
+            Console.ReadLine();
+        }
+    }
+}
