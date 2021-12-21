e1ed08ff7c2286acc3fe7f51ca5bead974a5452f
Alphiletti <67262963+Alphiletti@users.noreply.github.com>
2021-12-21 15:27:38 +0300

    Add files via upload

---
+++ metot tanımlama/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+           //Console.WriteLine(a+b);
+
+           int sonuc = Topla(a,b);
+           //Console.WriteLine(sonuc);
+
+           Metotlar M1 = new Metotlar();
+           M1.EkranaYazdir(sonuc.ToString());
+
+           int sonuc2 = M1.ArttırTopla(ref a, ref b);
+           M1.EkranaYazdir(Convert.ToString(sonuc2));
+           Console.ReadLine();
+        }
+
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(String veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttırTopla(ref int deger1, ref int deger2)
+        {
+            deger1+=1;
+            deger2+=1;
+            return deger1 + deger2;
+        }
+
+    }
+}
