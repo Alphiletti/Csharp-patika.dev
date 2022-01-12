815b90cb3420c6d3a6712894b259eba37429923d
Alphiletti <67262963+Alphiletti@users.noreply.github.com>
2022-01-12 10:06:28 +0300

    Add files via upload

---
+++ W11Database/W11Database/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿
+namespace W11Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.artIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.museumIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artcategoryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artistDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1 = new W11Database.DataSet1();
+            this.artTableAdapter = new W11Database.DataSet1TableAdapters.ArtTableAdapter();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.museumIDDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.numberofworksDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.museumBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.museumTableAdapter = new W11Database.DataSet1TableAdapters.MuseumTableAdapter();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.museumBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.artBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.museumBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.museumBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(302, 370);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(302, 445);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(302, 520);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 56);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.artIDDataGridViewTextBoxColumn,
+            this.museumIDDataGridViewTextBoxColumn,
+            this.artcategoryDataGridViewTextBoxColumn,
+            this.artistDataGridViewTextBoxColumn,
+            this.artNameDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.artBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(543, 312);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // artIDDataGridViewTextBoxColumn
+            // 
+            this.artIDDataGridViewTextBoxColumn.DataPropertyName = "Art_ID";
+            this.artIDDataGridViewTextBoxColumn.HeaderText = "Art_ID";
+            this.artIDDataGridViewTextBoxColumn.Name = "artIDDataGridViewTextBoxColumn";
+            this.artIDDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // museumIDDataGridViewTextBoxColumn
+            // 
+            this.museumIDDataGridViewTextBoxColumn.DataPropertyName = "Museum_ID";
+            this.museumIDDataGridViewTextBoxColumn.HeaderText = "Museum_ID";
+            this.museumIDDataGridViewTextBoxColumn.Name = "museumIDDataGridViewTextBoxColumn";
+            // 
+            // artcategoryDataGridViewTextBoxColumn
+            // 
+            this.artcategoryDataGridViewTextBoxColumn.DataPropertyName = "Art_category";
+            this.artcategoryDataGridViewTextBoxColumn.HeaderText = "Art_category";
+            this.artcategoryDataGridViewTextBoxColumn.Name = "artcategoryDataGridViewTextBoxColumn";
+            // 
+            // artistDataGridViewTextBoxColumn
+            // 
+            this.artistDataGridViewTextBoxColumn.DataPropertyName = "Artist";
+            this.artistDataGridViewTextBoxColumn.HeaderText = "Artist";
+            this.artistDataGridViewTextBoxColumn.Name = "artistDataGridViewTextBoxColumn";
+            // 
+            // artNameDataGridViewTextBoxColumn
+            // 
+            this.artNameDataGridViewTextBoxColumn.DataPropertyName = "Art_Name";
+            this.artNameDataGridViewTextBoxColumn.HeaderText = "Art_Name";
+            this.artNameDataGridViewTextBoxColumn.Name = "artNameDataGridViewTextBoxColumn";
+            // 
+            // artBindingSource
+            // 
+            this.artBindingSource.DataMember = "Art";
+            this.artBindingSource.DataSource = this.dataSet1;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // artTableAdapter
+            // 
+            this.artTableAdapter.ClearBeforeFill = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.museumIDDataGridViewTextBoxColumn1,
+            this.nameDataGridViewTextBoxColumn,
+            this.cityDataGridViewTextBoxColumn,
+            this.numberofworksDataGridViewTextBoxColumn});
+            this.dataGridView2.DataSource = this.museumBindingSource;
+            this.dataGridView2.Location = new System.Drawing.Point(616, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(443, 312);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // museumIDDataGridViewTextBoxColumn1
+            // 
+            this.museumIDDataGridViewTextBoxColumn1.DataPropertyName = "Museum_ID";
+            this.museumIDDataGridViewTextBoxColumn1.HeaderText = "Museum_ID";
+            this.museumIDDataGridViewTextBoxColumn1.Name = "museumIDDataGridViewTextBoxColumn1";
+            this.museumIDDataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // cityDataGridViewTextBoxColumn
+            // 
+            this.cityDataGridViewTextBoxColumn.DataPropertyName = "City";
+            this.cityDataGridViewTextBoxColumn.HeaderText = "City";
+            this.cityDataGridViewTextBoxColumn.Name = "cityDataGridViewTextBoxColumn";
+            // 
+            // numberofworksDataGridViewTextBoxColumn
+            // 
+            this.numberofworksDataGridViewTextBoxColumn.DataPropertyName = "Number_of_works";
+            this.numberofworksDataGridViewTextBoxColumn.HeaderText = "Number_of_works";
+            this.numberofworksDataGridViewTextBoxColumn.Name = "numberofworksDataGridViewTextBoxColumn";
+            // 
+            // museumBindingSource
+            // 
+            this.museumBindingSource.DataMember = "Museum";
+            this.museumBindingSource.DataSource = this.dataSet1;
+            // 
+            // museumTableAdapter
+            // 
+            this.museumTableAdapter.ClearBeforeFill = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 389);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 429);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(126, 464);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(122, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 392);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Artist:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.museumBindingSource1;
+            this.comboBox1.DisplayMember = "Name";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(126, 504);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(122, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.ValueMember = "Museum_ID";
+            // 
+            // museumBindingSource1
+            // 
+            this.museumBindingSource1.DataMember = "Museum";
+            this.museumBindingSource1.DataSource = this.dataSet1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 432);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Art Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 467);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Art Category:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 507);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Museum:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 625);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.artBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.museumBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.museumBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource artBindingSource;
+        private DataSet1TableAdapters.ArtTableAdapter artTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn artIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn museumIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn artcategoryDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn artistDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn artNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.BindingSource museumBindingSource;
+        private DataSet1TableAdapters.MuseumTableAdapter museumTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn museumIDDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cityDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn numberofworksDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.BindingSource museumBindingSource1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ W11Database/W11Database/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W11Database
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataSet1.Museum' table. You can move, or remove it, as needed.
+            this.museumTableAdapter.Fill(this.dataSet1.Museum);
+            // TODO: This line of code loads data into the 'dataSet1.Art' table. You can move, or remove it, as needed.
+            this.artTableAdapter.Fill(this.dataSet1.Art);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int museum;
+
+            museum = (int)comboBox1.SelectedValue;
+            this.artTableAdapter.Insert(museum,textBox3.Text,textBox1.Text,textBox2.Text);
+            MessageBox.Show("Saved");
+            this.artTableAdapter.Fill(this.dataSet1.Art);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int IDX;
+            int Sel_ID = 0;
+            IDX = dataGridView1.CurrentRow.Index;
+            int.TryParse(dataGridView1.Rows[IDX].Cells[0].Value.ToString(), out Sel_ID);
+            this.artTableAdapter.MyDelete(Sel_ID);
+            MessageBox.Show("Deleted");
+            this.artTableAdapter.Fill(this.dataSet1.Art);
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int IDX,museum;
+            int Sel_ID = 0;
+            IDX = dataGridView1.CurrentRow.Index;
+            museum = (int)comboBox1.SelectedValue;
+            int.TryParse(dataGridView1.Rows[IDX].Cells[0].Value.ToString(), out Sel_ID);
+            this.artTableAdapter.UpdateQuery(museum, textBox3.Text, textBox1.Text,textBox2.Text,Sel_ID,0);
+            MessageBox.Show("Updated");
+            this.artTableAdapter.Fill(this.dataSet1.Art);
+        }
+    }
+}
